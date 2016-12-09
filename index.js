f0cfec373c9091adf56f6e9568fa27fce8fc6102
Sarada Annarapa <sarada.annarapa@dhigroupinc.com>
2016-12-09 11:49:20 -0600

    v2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+var http = require("http");
+var url = require("url");
+http.createServer(function(req, res){
+	var queryData = url.parse(req.url,true).query;
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	if (queryData.id)
+	{
+		res.end('Hello User:' + queryData.id + '\n');
+	}
+	else
+	{
+		res.end('Unknown Request\n');
+	}
+	res.end('Hello World');
+}).listen(8081);
+
+console.log("Server Runnning");
+
