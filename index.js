40793415eb94038da17b76cb636f920258e6767e
Sarada Annarapa <sarada.annarapa@dhigroupinc.com>
2016-12-09 12:34:21 -0600

    v5

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
+var UserDb = require("./users.json");
 
 app.get('/users/', function(req, res) {
-	res.status(200).send("Hello Users");	
+	res.type('json').status(200).send(UserDb);	
 });
 
 app.get('/users/:id', function(req, res) { 
-	res.status(200).send("Hello User ID" + req.params.id);
+     var user = UserDb.filter(function(obj) { return obj.id == req.params.id})[0];
+	 if (user != null)
+	 {
+		res.type('json').status(200).send(user);	
+	 }
+	 else
+	 {
+		 res.status(404).send("404: User not found");
+	 }
 });
 
 app.use("*", function(req, res) { 
