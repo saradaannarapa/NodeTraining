af4bc5746cf2a4b79da0c49b3bc5413a6d4389d6
Sarada Annarapa <sarada.annarapa@dhigroupinc.com>
2016-12-09 12:20:50 -0600

    v4

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res) { 
-res.send("hello world");
-});
 
-app.get('/users', function(req, res) {
-	res.send("Hello Users");	
+app.get('/users/', function(req, res) {
+	res.status(200).send("Hello Users");	
 });
 
 app.get('/users/:id', function(req, res) { 
-res.send("Hello User ID" + req.params.id);
+	res.status(200).send("Hello User ID" + req.params.id);
 });
+
+app.use("*", function(req, res) { 
+res.status(404).send("404: Resource not found");
+})
 
 app.listen(8081, function() {console.log("live on prt 8081")})
 
